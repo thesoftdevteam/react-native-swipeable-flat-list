5b3c92b246c482a1d8fae7cb6d1947e57e1b2999
Ole Walkenhorst <git@lkdlr.eu>
2019-05-14 23:51:38 +0200

    Update react and react-native versions

--- example/index.android.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-
-import SwipeableFlatListExample from './src/App';
-
-AppRegistry.registerComponent('SwipeableFlatListExample', () => SwipeableFlatListExample);
